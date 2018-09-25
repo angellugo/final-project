729944fa8fd599c52f3d154ddda238517e802b30
Shane Chambry <Shaneechambry@gmail.com>
2018-09-24 19:34:29 -0700

    added eslint

--- server.js
+++ server.js
@@ -1,28 +1,25 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
-var mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-var db = require("./models");
-mongoose.connect("mongodb://localhost/project3LocalDB");
+const db = require('./models');
+mongoose.connect('mongodb://localhost/project3LocalDB');
 
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 }
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
 
-db.Test.create({ testData: "akfgkjanksd" });
-
-
-
+db.Test.create({testData: 'akfgkjanksd'});
