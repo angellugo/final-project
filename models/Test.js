7fc7b10baa529bb9301ea2a80bb6770c2c09d63e
Angel Lugo <this@angellugo.com>
2018-09-25 12:09:46 -0700

    Updated code to be ES6

--- models/Test.js
+++ models/Test.js
@@ -1,19 +1,19 @@
-var mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 // Save a reference to the Schema constructor
-var Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 // Using the Schema constructor, create a new UserSchema object
 // This is similar to a Sequelize model
-var TestSchema = new Schema({
+const TestSchema = new Schema({
   // `title` is required and of type String
   testData: {
     type: String,
-  }
+  },
 });
 
 // This creates our model from the above schema, using mongoose's model method
-var Test = mongoose.model("Test", TestSchema);
+const Test = mongoose.model('Test', TestSchema);
 
 // Export the Article model
 module.exports = Test;
