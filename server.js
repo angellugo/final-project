0229b68f0308c9ac49bae9a54df96b5e954a7db1
Shane Chambry <Shaneechambry@gmail.com>
2018-10-01 16:55:27 -0700

    refactor routes

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const routes = require('./routes');
-const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require('mongoose');
@@ -23,9 +22,6 @@ app.use(bodyParser.json());
 app.use(routes);
 // Send every request to the React app
 // Define any API routes before this runs
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
-});
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
