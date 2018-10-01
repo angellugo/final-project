9d3ff3ceb9c3a5023650f3315d607b8a4ec4a4c3
Angel Lugo <this@angellugo.com>
2018-10-01 16:25:19 -0700

    Added "sourceType": "module" to the eslint file to fix the imports error

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
     "extends": "google",
     "parserOptions": {
         "ecmaVersion": 6,
+        "sourceType": "module",
+        "parser": "babel-eslint"
     },
     "rules": {
         "new-cap": ["error", { "capIsNewExceptions": ["Router"] }]
